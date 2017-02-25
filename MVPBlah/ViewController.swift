3c34c5782d925ab653b82370efa5d658c090a7f0
simonecaldon <simone.caldon@gmail.com>
2017-02-25 23:13:52 +0100

    meh

--- MVPBlah/ViewController.swift
+++ MVPBlah/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import RxSwift
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var sampleTableView: UITableView!
+    let textCellIdentifier = "TextCell"
+    let tableData = ["loading"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sampleTableView.delegate = self
+        sampleTableView.dataSource = self
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: textCellIdentifier, for: indexPath as IndexPath)
+        
+        let row = indexPath.row
+        cell.textLabel?.text = tableData[row]
+        
+        return cell
+    }
+    
+    
 }
 
---
+++ Pods/Target Support Files/Pods-MVPBlahUITests/Pods-MVPBlahUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MVPBlahUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MVPBlahUITests
+@end
