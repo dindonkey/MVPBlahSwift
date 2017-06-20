ff781329c7552605557fa73ef267ffc550d307b3
dindonkey <antani@trovaprezzi.it>
2017-06-20 14:38:26 +0200

    refactor with page object

--- MVPBlahUITests/HomePage.swift
+++ MVPBlahUITests/HomePage.swift
@@ -6,14 +6,19 @@
 //  Copyright © 2017 Simone Caldon. All rights reserved.
 //
 
+import XCTest
 import Foundation
 import MockWebServer
 
-class HomePage {
-    var mockWebServer: MockWebServer?
+public class HomePage {
+    let app = XCUIApplication()
+    let mockWebServer = MockWebServer()
     
-    func launchHome() {
-        
+    func launch() {
+        mockWebServer.start(9000)
+        app.launchEnvironment[AppDelegate.USE_IMMEDIATE_SCHEDULERS] = "YES"
+        app.launchEnvironment[AppDelegate.USE_MOCK_WEB_SERVER] = "YES"
+        app.launch()
     }
     
     func enqueueJsonResoponse(_ filePath: String)
@@ -25,8 +30,21 @@ class HomePage {
         
         let dispatchMap = DispatchMap()
         dispatchMap.add(jokesDispatch)
-        mockWebServer!.setDispatch(dispatchMap)
+        mockWebServer.setDispatch(dispatchMap)
 
     }
+    
+    func checkTextIsDisplayed(_ text: String)
+    {
+        XCTAssert(app.staticTexts.element(matching: NSPredicate(format: "label CONTAINS '" + text + "'")).exists)
+    }
+    
+    func shutdown()
+    {
+        mockWebServer.stop()
+    }
+    
+    // useful for debug:
+    // print(XCUIApplication().debugDescription)
     
 }
--- MVPBlahUITests/MVPBlahUITests.swift
+++ MVPBlahUITests/MVPBlahUITests.swift
@@ -11,50 +11,23 @@ import MockWebServer
 
 class MVPBlahUITests: XCTestCase {
     
-    let app = XCUIApplication()
-    var mockWebServer: MockWebServer?
-
-
+    let homePage = HomePage()
+    
     override func setUp() {
         super.setUp()
         continueAfterFailure = false
-
-        mockWebServer = MockWebServer()
-        mockWebServer!.start(9000)
     }
     
     override func tearDown() {
         super.tearDown()
-        mockWebServer!.stop()
+        homePage.shutdown()
     }
-
-    //non ha più senso, in teoria se usi immediate non vedi lo stato di caricamento
-//    func testLoadingIsShown() {
-//        app.launch()
-//
-//        XCTAssert(app.staticTexts["loading"].exists)
-//    }
     
     func testLoadedDataIsShow() {
-
-        let jokesDispatch: Dispatch = Dispatch()
-        jokesDispatch.requestContain("jokes/random")
-                .setResponseCode(200)
-                .responseBody(for: Bundle(for: object_getClass(self)), fromFile: "jokes.json")
-
-        let dispatchMap = DispatchMap()
-        dispatchMap.add(jokesDispatch)
-        mockWebServer!.setDispatch(dispatchMap)
-
-
-        app.launchEnvironment[AppDelegate.USE_IMMEDIATE_SCHEDULERS] = "YES"
-        app.launchEnvironment[AppDelegate.USE_MOCK_WEB_SERVER] = "YES"
-        app.launch()
-
-        XCTAssert(app.staticTexts.element(matching: NSPredicate(format: "label CONTAINS 'CNN'")).exists)
-
+        
+        homePage.enqueueJsonResoponse("jokes.json")
+        homePage.launch()
+        homePage.checkTextIsDisplayed("CNN")
     }
-    
-    // useful for debug:
-    // print(XCUIApplication().debugDescription)
+
 }
